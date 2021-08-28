470a7dd25d757b1921432c1efdb72cc7a0b04d49
viratsharma56 <viratsharma5129@gmail.com>
2021-08-28 21:43:47 +0530

    First commit

---
+++ src/components/News.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import Loading from "./Loading";
+import NewsItem from "./NewsItem";
+
+export class News extends Component {
+    static defaultProps = {
+        country: 'in',
+        pageSize: 8,
+        category: 'general'
+    }
+
+    constructor() {
+        super();
+        this.state = {
+        articles: [],
+        loading: false,
+        page: 1,
+        };
+  }
+
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ac577cbfd4184e938a3a44839400e24f&page=1&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+    });
+  }
+
+  handlePrev = async () => {
+    this.setState({ loading: true });
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ac577cbfd4184e938a3a44839400e24f&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page - 1,
+      loading: false,
+    });
+  };
+
+  handleNext = async () => {
+    this.setState({ loading: true });
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ac577cbfd4184e938a3a44839400e24f&page=${
+      this.state.page + 1
+    }&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page + 1,
+      loading: false,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container my-3">
+        <h2 className="text-center">NewsMonkey</h2>
+        {this.state.loading && <Loading />}
+        <div className="row">
+          {!this.state.loading && this.state.articles.map((e) => {
+            return (
+              <div className="col-md-4" key={e.url}>
+                <NewsItem
+                  title={e.title ? e.title.slice(0, 50) : ""}
+                  description={e.description ? e.description.slice(0, 88) : ""}
+                  imgurl={e.urlToImage}
+                  newsurl={e.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="container d-flex justify-content-between my-4">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            class="btn btn-dark"
+            onClick={this.handlePrev}
+          >
+            &larr; Previous
+          </button>
+          <button type="button" class="btn btn-dark" onClick={this.handleNext}>
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
