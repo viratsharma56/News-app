040c07a68f307d5a5ab20ae855c889852137b2a2
viratsharma56 <viratsharma5129@gmail.com>
2021-08-30 09:53:28 +0530

    Doing some random changes

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Loading from "./Loading";
 import NewsItem from "./NewsItem";
 import InfiniteScroll from "react-infinite-scroll-component";
+import PropTypes from 'prop-types'
 
 export class News extends Component {
   static defaultProps = {
@@ -9,6 +10,12 @@ export class News extends Component {
     pageSize: 8,
     category: "general",
   };
+
+  static propTypes={
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
 
   capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -22,57 +29,41 @@ export class News extends Component {
       page: 1,
       totalResult: 0,
     };
-    document.title = `${this.capitalizeFirstLetter(
-      this.props.category
-    )}-News Monkey`;
+    document.title = `${this.capitalizeFirstLetter(this.props.category)}-News Monkey`;
+  }
+
+  async updateNews() {
+    this.props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    this.props.setProgress(30);
+    let parsedData = await data.json()
+    this.props.setProgress(70);
+    this.setState({
+        articles: parsedData.articles,
+        totalResults: parsedData.totalResults,
+        loading: false, 
+    })
+    this.props.setProgress(100);
   }
 
   async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=1&pageSize=${this.props.pageSize}`;
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      articles: parsedData.articles,
-    });
+    this.updateNews();
   }
 
   handlePrev = async () => {
-    this.setState({ loading: true });
-    let url = `https://newsapi.org/v2/top-headlines?country=${
-      this.props.country
-    }&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${
-      this.state.page - 1
-    }&pageSize=${this.props.pageSize}`;
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      articles: parsedData.articles,
-      page: this.state.page - 1,
-      loading: false,
-      totalResult: parsedData.totalResults,
-    });
+    this.setState({page: this.state.page - 1});
+    this.updateNews();
   };
 
   handleNext = async () => {
-    this.setState({ loading: true });
-    let url = `https://newsapi.org/v2/top-headlines?country=${
-      this.props.country
-    }&category=${
-      this.props.category
-    }&apiKey=${this.props.apiKey}&page=${
-      this.state.page + 1
-    }&pageSize=${this.props.pageSize}`;
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      articles: parsedData.articles,
-      page: this.state.page + 1,
-      loading: false,
-      totalResult: parsedData.totalResults,
-    });
+    this.setState({page: this.state.page + 1});
+    this.updateNews();
   };
 
   fetchMoreData = async () => {
+    this.setState({page: this.state.page + 1});
     let url = `https://newsapi.org/v2/top-headlines?country=${
       this.props.country
     }&category=${
@@ -84,7 +75,6 @@ export class News extends Component {
     let parsedData = await data.json();
     this.setState({
       articles: this.state.articles.concat(parsedData.articles),
-      page: this.state.page + 1,
       loading: false,
       totalResult: parsedData.totalResults,
     });
@@ -120,23 +110,6 @@ export class News extends Component {
             })}
           </div>
         </InfiniteScroll>
-        {/* <div className="container d-flex justify-content-between my-4">
-          <button
-            disabled={this.state.page <= 1}
-            type="button"
-            className="btn btn-dark"
-            onClick={this.handlePrev}
-          >
-            &larr; Previous
-          </button>
-          <button
-            type="button"
-            className="btn btn-dark"
-            onClick={this.handleNext}
-          >
-            Next &rarr;
-          </button>
-        </div> */}
       </div>
     );
   }
