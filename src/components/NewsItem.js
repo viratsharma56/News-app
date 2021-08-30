891dd8458506edcb913d5c8b7f0f7a33c51c1cd4
viratsharma56 <viratsharma5129@gmail.com>
2021-08-30 10:04:49 +0530

    Final change in class based components

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,20 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
-    render() {
-        let { title, description, imgurl, newsurl, author, date } = this.props;
-        return (
-            <div className="card">
-                <img src={imgurl} className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{description}</p>
-                    <p className="card-text"><small className="text-muted">By {author ? author : "anonymous"} on {date}</small></p>
-                    <a rel="noreferrer" href={newsurl} target="_blank" className="btn btn-sm btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    let { title, description, imgurl, newsurl, author, date, source } =
+      this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className="badge rounded-pill bg-danger"> {source} </span>
+          </div>
+          <div className="card-body">
+            <img
+              src={
+                !imgurl
+                  ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg"
+                  : imgurl
+              }
+              className="card-img-top"
+              alt="..."
+            />
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {author ? author : "anonymous"} on {date}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsurl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default NewsItem
+export default NewsItem;
